4dcbb840f422ece8ff67e2f5cca4411949904fba
Jacky20051118 <kongchingkit@gmail.com>
2026-09-26 02:43:20 +0800

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,20 @@
+public class Shape {
+    private double x, y;
+
+    public Shape(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    // base on class and no actual formula yet
+    public double perimeter() {
+        return 0;
+    }
+
+    public double area() {
+        return 0;
+    }
+}
